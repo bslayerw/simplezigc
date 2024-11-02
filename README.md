ac22e4b892b0133cb8ab066f351f5c336686dd4b
Byron Wright <bslayerw@gmail.com>
2024-11-02 15:32:56 -0400

    lets get it started.

---
+++ README.md
@@ -0,0 +1,10 @@
+# csetup
+
+A simple template to start a c project using zig as the build system.
+
+## Building
+
+- `zig build`
+
+## TODO
+
---
+++ build.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const exe = b.addExecutable(.{
+        .name = "mgr",
+        .target = target,
+    });
+
+    exe.addCSourceFile(.{
+        .file = b.path("main.c"),
+        .flags = &[_][]const u8{},
+    });
+
+    exe.linkLibC();
+    exe.linkSystemLibrary("libsystemd");
+    b.installArtifact(exe);
+}
